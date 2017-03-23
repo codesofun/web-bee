20621dcc20c216f3f2c94257902348f7085ad264
庄文达 <pkwenda@163.com>
2017-03-23 23:51:01 +0800

    add maven Log4j
    add maven Log4j

--- src/main/java/bee/download/HttpClientDownloader.java
+++ src/main/java/bee/download/HttpClientDownloader.java
@@ -1,10 +1,21 @@
 package bee.download;
 
+import bee.Bee;
+import bee.linker.Page;
+import bee.linker.Request;
+
+import java.util.logging.Logger;
+
 /**
  * data 2017-03-23   23:43
  * E-mail   sis.nonacosa@gmail.com
  * @author sis.nonacosa
  */
-public class HttpClientDownloader {
+public class HttpClientDownloader implements DownLoader{
+    private Logger logger = Logger.getLogger(String.valueOf(getClass()));
 
+    @Override
+    public Page download(Request request, Bee bee) {
+
+    }
 }
