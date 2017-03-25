47c4a0a76c755fb004ec5e50b1eecf95f164352b
庄文达 <pkwenda@163.com>
2017-03-25 16:56:12 +0800

    create httpRequest  httpResponse
    create httpRequest  httpResponse

---
+++ src/main/java/bee_core/HttpClient/HttpRequest.java
@@ -0,0 +1,12 @@
+package bee_core.HttpClient;
+
+
+/**
+ * 封装HTTP请求
+ *
+ * data 2017-03-25   16:54
+ * E-mail   sis.nonacosa@gmail.com
+ * @author sis.nonacosa
+ */
+public class HttpRequest {
+}
---
+++ src/main/java/bee_core/HttpClient/HttpResponse.java
@@ -0,0 +1,11 @@
+package bee_core.HttpClient;
+
+/**
+ * 封装HTTP响应
+ * 
+ * data 2017-03-25   16:55
+ * E-mail   sis.nonacosa@gmail.com
+ * @author sis.nonacosa
+ */
+public class HttpResponse {
+}
