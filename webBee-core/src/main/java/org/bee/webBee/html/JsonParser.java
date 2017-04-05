b6acabebab4ec9f66dde208365567db7d87c873c
庄文达 <pkwenda@163.com>
2017-04-05 11:41:28 +0800

    json parser
    json parser

--- webBee-core/src/main/java/org/bee/webBee/html/JsonParser.java
+++ webBee-core/src/main/java/org/bee/webBee/html/JsonParser.java
@@ -1,10 +1,10 @@
 package org.bee.webBee.html;
 
 /**
- * data 2017-04-01   17:42
+ * data 2017-04-01    17:42
  * E-mail   sis.nonacosa@gmail.com
  * @author sis.nonacosa
  */
 public interface JsonParser {
-    
+
 }
