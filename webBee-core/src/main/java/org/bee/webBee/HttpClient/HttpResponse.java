a2a2e874e18ca83bd67bc7481805af3974db5813
庄文达 <pkwenda@163.com>
2017-05-11 01:24:48 +0800

    resolve HttpResponse
    resolve HttpResponse

--- webBee-core/src/main/java/org/bee/webBee/HttpClient/HttpResponse.java
+++ webBee-core/src/main/java/org/bee/webBee/HttpClient/HttpResponse.java
@@ -1,6 +1,15 @@
 package org.bee.webBee.HttpClient;
 
+
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.bee.webBee.linker.Request;
+import org.bee.webBee.processor.Setting;
+import org.bee.webBee.processor.Task;
+
+import java.io.IOException;
 
 /**
  * 封装HTTP响应
@@ -10,4 +19,34 @@ import org.apache.http.client.methods.CloseableHttpResponse;
  * @author sis.nonacosa
  */
 public class HttpResponse {
+
+    private Request request;
+
+    private Task task;
+
+    public HttpResponse(Request request, Task task){
+        this.request = request;
+        this.task = task;
+    }
+    public CloseableHttpResponse getResponse()   {
+        CloseableHttpResponse closeableHttpResponse = null;
+        try {
+            Setting setting = task.getSetting();
+
+            HttpRequestBase httpMethod ;
+            httpMethod = HttpClientPool.getInstance().generateHttpMethod(request,task,null);
+
+            HttpClientBuilder httpClientBuilder = HttpClientPool.getInstance().generateClient(setting,httpMethod);
+            CloseableHttpClient closeableHttpClient = httpClientBuilder.build();
+             closeableHttpResponse = closeableHttpClient.execute(httpMethod);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println(e);
+        }
+        return closeableHttpResponse;
+    }
+
+    public static HttpResponse getInstance(Request request, Task task){
+        return new HttpResponse(request,task);
+    }
 }
