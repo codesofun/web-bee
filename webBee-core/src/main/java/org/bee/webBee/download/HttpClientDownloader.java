85370300c8f8940096421789cc9fbb4da5e571db
庄文达 <pkwenda@163.com>
2017-05-11 01:59:09 +0800

    add task queue to Page
    add task queue to Page

--- webBee-core/src/main/java/org/bee/webBee/download/HttpClientDownloader.java
+++ webBee-core/src/main/java/org/bee/webBee/download/HttpClientDownloader.java
@@ -3,6 +3,7 @@ package org.bee.webBee.download;
 import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.util.EntityUtils;
 import org.bee.webBee.HttpClient.HttpClientPool;
+import org.bee.webBee.HttpClient.HttpResponse;
 import org.bee.webBee.html.Html;
 import org.bee.webBee.linker.Page;
 import org.bee.webBee.linker.Request;
@@ -12,7 +13,10 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.logging.Logger;
 
 /**
@@ -26,20 +30,15 @@ public class HttpClientDownloader implements DownLoader {
 //    private Page page = new Page();
     @Override
     public Page download(Request request, Task task) {
-        Setting setting = task.getSetting();
-
-        HttpRequestBase httpMethod =  null;
-        httpMethod = HttpClientPool.getInstance().generateHttpMethod(request,task,httpMethod);
-
-        HttpClientBuilder httpClientBuilder = HttpClientPool.getInstance().generateClient(setting,httpMethod);
-        CloseableHttpClient closeableHttpClient = httpClientBuilder.build();
+        CloseableHttpResponse closeableHttpResponse  = HttpResponse.getInstance(request,task).getResponse();
         Page page = new Page();
         try {
-            CloseableHttpResponse closeableHttpResponse = closeableHttpClient.execute(httpMethod);
             System.out.println(closeableHttpResponse.getStatusLine().getStatusCode());
             System.out.println(request.getUrl());
+
             page.setHtml(closeableHttpResponse);
             page.setRequest(request);
+            page.setTast(task);
             page.setUrl(request.getUrl());
             page.setStatusCode(closeableHttpResponse.getStatusLine().getStatusCode());
             task.setHtml(page.getHtml()); //自传递api到Bee处理器
@@ -53,7 +52,5 @@ public class HttpClientDownloader implements DownLoader {
         return page;
     }
 
-//    public Html getHtml(){
-//        return page.getHtml();
-//    }
+
 }
