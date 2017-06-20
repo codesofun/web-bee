4c4495096d182c0a3467aec58504d9fb3a59a8bc
庄文达 <pkwenda@163.com>
2017-06-21 00:05:52 +0800

    move test by Excutor
    move test by Excutor

--- webBee-core/src/test/java/thread/Collable.java
+++ webBee-core/src/test/java/thread/Collable.java
@@ -1,4 +1,4 @@
-package webbee.executor;
+package thread;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
--- webBee-core/src/test/java/thread/Executor.java
+++ webBee-core/src/test/java/thread/Executor.java
@@ -1,4 +1,4 @@
-package webbee.executor;
+package thread;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
